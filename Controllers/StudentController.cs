945df9777492e64824e928b0bb2d2f9d0a446cc8
nhatnguyennhn <nhat.nguyen.nhn@gmail.com>
2021-03-15 11:35:53 +0700

    BT API

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using NguyenHoangNhat_5951071069.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NguyenHoangNhat_5951071069.Controllers
+{
+    public class StudentController : ApiController
+    {
+        public StudentInfo Get()
+        {
+            var Nhatcute= new StudentInfo
+            {
+                TenSinhVien = $"Nguyen Hoang Nhat",
+                MaSinhVien = $"5951071069",
+                NgaySinh = $"02/01/2000",
+                QueQuan = $"Ha Tinh"
+
+            };
+            return Nhatcute;
+        }
+
+    }
+}
---
+++ Models/StudentInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace NguyenHoangNhat_5951071069.Models
+{
+    [DataContract]
+    public class StudentInfo
+    {
+        [DataMember(Name = "tenSinhVien")]
+        public string TenSinhVien { get; set; }
+        [DataMember(Name = "maSinhVien")]
+        public string MaSinhVien { get; set; }
+        [DataMember(Name = "ngaySinh")]
+        public string NgaySinh { get; set; }
+        [DataMember(Name = "queQuan")]
+        public string QueQuan { get; set; }
+
+    }
+}
